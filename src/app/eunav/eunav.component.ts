8dff825b338adedc624b9f3135d186b65c38a889
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2017-05-30 17:58:24 +0000

    EuNavComponent setup, css, html

---
+++ src/app/eunav/eunav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'eu-nav',
+  templateUrl: './eunav.component.html',
+  styleUrls: ['./eunav.component.css']
+})
+
+export class EuNavComponent {
+
+}
