82427199c59c396aee403dc582743dc7dd364d99
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2017-06-02 22:45:01 +0000

    EuNavComponent slide effect

--- src/app/eunav/eunav.component.ts
+++ src/app/eunav/eunav.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+class EuButton {
+  link: string;
+  link_path: string;
+  is_active = false;
+  constructor(linkname: string) {
+    this.link = linkname;
+  }
+}
+
 @Component({
   selector: 'eu-nav',
   templateUrl: './eunav.component.html',
@@ -7,5 +16,14 @@ import { Component } from '@angular/core';
 })
 
 export class EuNavComponent {
-
+  eubuttons: EuButton[] = [
+    new EuButton('About'),
+    new EuButton('Programming'),
+    new EuButton('Photography'),
+    new EuButton('Climbing'),
+  ];
+  nav_state = 'open';
+  toggle_nav() {
+    this.nav_state = (this.nav_state === 'open' ? 'closed' : 'open');
+  }
 }
