b65dd93e660e0cea842e887b36f21d139245aa25
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2017-06-16 12:35:07 -0700

    File loader, skill icons and links

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,10 @@ module.exports = {
         loader: 'html-loader'
       },
       {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: 'file-loader?name=assets/[name].[hash].[ext]'
+      },
+      {
         test: /\.css$/,
         loader: 'raw-loader'
       }
