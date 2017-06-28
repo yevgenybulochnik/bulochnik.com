f95566ae1d3d7d1c8c23d7e595ca6a07a6a0c84a
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2017-06-27 20:50:57 -0700

    EuEditorComponent html template

--- src/app/eueditor/eueditor.component.ts
+++ src/app/eueditor/eueditor.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'eu-editor',
-  template: `
-  <div> EuEditor Component
-    <risk-calc></risk-calc>
-  </div>
-  `,
+  templateUrl: './eueditor.component.html',
   styles: [`
     div {
       border: solid red 1px;
