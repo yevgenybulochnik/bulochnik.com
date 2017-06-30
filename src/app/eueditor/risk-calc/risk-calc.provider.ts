2b8cd6ad696bf95608ef35b940416be32a13b61a
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2017-06-30 13:15:45 -0700

    Risk-Calc provider

---
+++ src/app/eueditor/risk-calc/risk-calc.provider.ts
@@ -0,0 +1,75 @@
+export class ChadsVasc {
+    riskcalc_name: string;
+    riskfactors: RiskFactor[];
+    scores: string[];
+    constructor() {
+        this.riskcalc_name = 'CHADS-Vasc';
+        this.riskfactors = [
+            new RiskFactor('Congestive Heart Failure', 'CHF', 1),
+            new RiskFactor('Hypertension', 'HTN', 1),
+            new RiskFactor('Age between 65 and 75', 'AGE 65-75', 1),
+            new RiskFactor('Age over 75', 'AGE >75', 2),
+            new RiskFactor('Diabetes', 'DM', 1),
+            new RiskFactor('CVA/TIA', 'CVA', 2),
+            new RiskFactor('Female', 'Female', 1),
+            new RiskFactor('Coronary Artery Disease', 'CAD', 1)
+            ];
+        this.scores = [
+            '',
+            '0.6%',
+            '2.2%',
+            '3.2%',
+            '4.8%',
+            '7.2%',
+            '9.7%',
+            '11.2%',
+            '10.8%',
+            '12.2%'
+            ]
+    }
+}
+
+export class HasBled {
+    riskcalc_name: string;
+    riskfactors: riskfactor[];
+    scores: string[];
+    constructor() {
+        this.riskcalc_name = 'HasBled';
+        this.riskfactors = [
+            new RiskFactor('Hypertension', 'HTN', 1),
+            new RiskFactor('Renal Failure', 'Renal', 1),
+            new RiskFactor('Liver Failure', 'Liver', 1),
+            new RiskFactor('CVA/TIA', 'CVA', 2),
+            new RiskFactor('Bleeding History', 'Bleed', 1),
+            new RiskFactor('Labile INR', 'Labile INR', 1),
+            new RiskFactor('Age > 65', 'Age >65', 1),
+            new RiskFactor('ETOH Intake', 'ETOH', 1),
+            new RiskFactor('Other Anticoagulants', 'Thinners', 1)
+            ];
+        this.scores = [
+             '',
+            '3.4%',
+            '4.1%',
+            '5.8%',
+            '8.9%',
+            '9.1%',
+            '>10%',
+            '>10%',
+            '>10%',
+            '>10%'
+        ]
+    }
+}
+
+class RiskFactor {
+    name: string;
+    abv: string;
+    value: number;
+    is_active: boolean;
+    constructor(name, abv, value) {
+        this.name = name;
+        this.abv = abv;
+        this.value = value;
+        this.is_active = false;
+    }
+}
