9e7167c949f0bac1ded31e06c5252d2df17d7672
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2017-07-12 22:42:21 -0700

    Refactored element scrollposition functions

--- src/app/eunav/eunav.component.ts
+++ src/app/eunav/eunav.component.ts
@@ -1,14 +1,21 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener} from '@angular/core';
 
 class EuLink {
   link: string;
   link_path: string;
   link_id: string;
   is_active = false;
+  scroll_position: number;
   constructor(linkname: string) {
     this.link = linkname;
     this.link_path = '/' + linkname.toLowerCase().replace(' ', '');
     this.link_id = '#' + linkname.toLowerCase();
+  }
+  get_scrollPosition() {
+    let elem = document.getElementById(this.link.toLowerCase())
+    if (elem) {
+      this.scroll_position = elem.offsetTop
+    }
   }
 }
 
@@ -44,32 +51,23 @@ export class EuNavComponent {
     new EuButton('Photography', ['Nature', 'Landscape', 'Portrait']),
     new EuButton('Climbing'),
   ];
-  scroll_positions: object;
   nav_state = 'open';
+  window_position: number;
   @HostListener('window:scroll') onScroll() {
     let window_position = window.pageYOffset
     console.log(window_position)
-    for (let button in this.scroll_positions) {
-      console.log(button, this.scroll_positions[button])
-    }
+    this.window_position = window_position
   }
   ngOnInit() {
-    let scrollable_links = []
     for (let i = 0; i < this.eubuttons.length; i++) {
-      scrollable_links.push(this.eubuttons[i].link.toLowerCase())
+      this.eubuttons[i].get_scrollPosition()
       if (this.eubuttons[i].sublink) {
         for (let n = 0; n < this.eubuttons[i].sublink.length; n++) {
-          scrollable_links.push(this.eubuttons[i].sublink[n].link.toLowerCase().replace(' ', ''))
+          this.eubuttons[i].sublink[n].get_scrollPosition()
         }
       }
     }
-    let link_dict = {}
-    for (let i = 0; i < scrollable_links.length; i++) {
-      if (document.getElementById(scrollable_links[i])) {
-        link_dict[scrollable_links[i]] = document.getElementById(scrollable_links[i]).offsetTop
-      }
-    }
-    this.scroll_positions = link_dict
+    console.log(this.eubuttons)
   }
   toggle_nav() {
     this.nav_state = (this.nav_state === 'open' ? 'closed' : 'open');
