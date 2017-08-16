bd40bfd4cbe4caab063704a90d2d31c26daf412b
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2017-08-16 19:53:06 +0000

    Fixed toolbar indentation

--- src/app/eueditor/eueditor.component.ts
+++ src/app/eueditor/eueditor.component.ts
@@ -13,13 +13,13 @@ export class EuEditorComponent {
   hasbled = new HasBled;
   chadsvasc = new ChadsVasc;
   toolbarButtons = {
-      toolbar: [
-        ['bold', 'italic', 'underline', 'strike'],
-        [{'list': 'ordered'}, {'list': 'bullet'}],
-        [{'indent': '-1'}, {'indent': '+1'}]
-        //[{'color': [] }, {'background': [] }]   *Note: causes implicit any warnings
-      ]
-    };
+    toolbar: [
+      ['bold', 'italic', 'underline', 'strike'],
+      [{'list': 'ordered'}, {'list': 'bullet'}],
+      [{'indent': '-1'}, {'indent': '+1'}]
+      //[{'color': [] }, {'background': [] }]   *Note: causes implicit any warnings
+    ]
+   };
   get_assessment(evt: string) {
     console.log(evt)
   }
